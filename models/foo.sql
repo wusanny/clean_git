281e1c7cb0862267691b58699c996bf919ae2ae5
Sanny Prawira <sanny.prawira@fishtownanalytics.com>
2025-06-10 05:25:12 +0000

    dd

--- models/foo.sql
+++ models/foo.sql
@@ -1,1 +1,9 @@
-select 1 as potato
+{% set q = 'select 1 as c' %}
+{% if execute %}
+{% set res = run_query(q) %}
+{% set a = res.columns[0].values() %}
+{% set b = res.columns[0].values()[0] %}
+{% endif %}
+
+-- {{ a }}
+-- {{ b }}
