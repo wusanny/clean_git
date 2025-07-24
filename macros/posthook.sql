ff3fbb81dd357cbca4be075068f5e49c2daad70f
Sanny Prawira <sanny.prawira@fishtownanalytics.com>
2025-07-24 07:52:12 +0000

    hh

--- macros/posthook.sql
+++ macros/posthook.sql
@@ -9,18 +9,23 @@
 
   {% set results = run_query(payment_method_query) %}
 
-  {# Return the first column #}
-  {% set payment_methods = results.columns[0].values() %}
-  
-  {{ log("=" * 50, info=True) }}
-  {{ log("POST HOOK: Payment Methods Analysis", info=True) }}
-  {{ log("=" * 50, info=True) }}
-  {{ log("Found " ~ payment_methods|length ~ " distinct payment methods:", info=True) }}
-  
-  {% for method in payment_methods %}
-    {{ log("  → " ~ method, info=True) }}
-  {% endfor %}
-  
-  {{ log("=" * 50, info=True) }}
+  {% if execute %}
+    {# Return the first column #}
+    {% set payment_methods = results.columns[0].values() %}
+    
+    {{ log("=" * 50, info=True) }}
+    {{ log("POST HOOK: Payment Methods Analysis for " ~ relation, info=True) }}
+    {{ log("=" * 50, info=True) }}
+    {{ log("Found " ~ payment_methods|length ~ " distinct payment methods:", info=True) }}
+    
+    {% for method in payment_methods %}
+      {{ log("  → " ~ method, info=True) }}
+    {% endfor %}
+    
+    {{ log("=" * 50, info=True) }}
+    
+  {% else %}
+    {% set payment_methods = [] %}
+  {% endif %}
 
 {% endmacro %}
